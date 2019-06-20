73bb6f0e1fc70f246edaac315a4445a6c4bd6f97
quentg77 <q.guichaoua@gmail.com>
2019-06-21 01:45:02 +0200

    add all missing table

--- models/index.js
+++ models/index.js
@@ -2,9 +2,20 @@ import Sequelize from "sequelize";
 import dotenv from "dotenv";
 
 import User from "./user";
+import Consume from "./consume";
+import Product from "./product";
+import Type from "./type";
 
 dotenv.config();
 
 export const db = new Sequelize(process.env.DATABASE_URL + "?" + process.env.DATABASE_ARG);
 
 User.init(db);
+Consume.init(db);
+Product.init(db);
+Type.init(db);
+
+Product.belongsTo(Type);
+
+Consume.belongsTo(User);
+Consume.belongsTo(Product);
---
+++ models/product.js
@@ -0,0 +1,40 @@
+import Sequelize, { Model } from "sequelize";
+
+export default class Product extends Model {
+    static init(database) {
+        return super.init(
+            {
+                id: {
+                    type: Sequelize.INTEGER,
+                    primaryKey: true,
+					autoIncrement: true
+                },
+                name: {
+					type: Sequelize.STRING,
+					allowNull: false,
+					validate: {
+						notEmpty: true
+					}
+                },
+                barcode: {
+					type: Sequelize.INTEGER,
+					allowNull: false,
+					validate: {
+						notEmpty: true
+					}
+                }
+            },
+            {
+                tableName: "product",
+                sequelize: database,
+
+                indexes: [
+					{
+						unique: true,
+						fields: ["id"]
+					}
+				]
+            }
+        )
+    }
+}
